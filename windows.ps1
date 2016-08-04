b0bbc743bcd27aedc434f16ac0e67421642777ac
Marc Trudel <mtrudel@wizcorp.jp>
2016-08-04 18:03:57 +0900

    windows.ps1: Force all commands

--- windows.ps1
+++ windows.ps1
@@ -1,11 +1,12 @@
 write-host "Set up chocolatey"
 get-packageprovider -name chocolatey -force -forcebootstrap
+get-packageprovider -name nuget -force -forcebootstrap
 
 write-host "Installing"
-install-package GoogleChrome
-install-package nodejs
-install-package nvm
-install-package atom
-install-package unity
-install-package VisualStudioCode
-install-package VisualStudio2015Community
+install-package GoogleChrome -force -forcebootstrap
+install-package nodejs -force -forcebootstrap
+install-package nvm -force -forcebootstrap
+install-package atom -force -forcebootstrap
+install-package unity -force -forcebootstrap
+install-package VisualStudioCode -force -forcebootstrap
+install-package VisualStudio2015Community -force -forcebootstrap
