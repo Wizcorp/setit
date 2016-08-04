6592043bdac3b63946f06c1a1b09e9e3500e140d
Marc Trudel <mtrudel@wizcorp.jp>
2016-08-04 19:48:56 +0900

    Don't delete the AdminDeployment.xml file

--- windows.ps1
+++ windows.ps1
@@ -14,12 +14,15 @@ install-package nvm -force -forcebootstrap
 install-package unity -force -forcebootstrap
 install-package git -force -forcebootstrap
 
+# Todo: fails at install time
+install-package cmder -force -forcebootstrap
+
+# VS2015Community
 Invoke-WebRequest -Uri https://download.microsoft.com/download/0/B/C/0BC321A4-013F-479C-84E6-4A2F90B11269/vs_community.exe -OutFile vs_community.exe
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/daptiv/visualstudio/master/files/default/2015/AdminDeployment-community.xml -OutFile admin.xml
 # /full instead of /AdminFile for full install
 .\vs_community.exe /quiet /passive /AdminFile admin.xml
 rm vs_community.exe
-rm admin.xml
 
-# Todo: fails at install time
-install-package cmder -force -forcebootstrap
+# VS install runs in background, we can't delete this for now!
+# rm admin.xml
